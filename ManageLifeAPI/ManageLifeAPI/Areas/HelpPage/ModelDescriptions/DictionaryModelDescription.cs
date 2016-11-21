8690ea561edcad9aec8e4d0b39871f12cd8e628f
Hari Krishna Jha <Hari.jha@rsystems.com>
2016-11-21 16:09:13 +0530

    Added intial project

---
+++ ManageLifeAPI/ManageLifeAPI/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ManageLifeAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ ManageLifeAPI/ManageLifeAPI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ManageLifeAPI.WebApiApplication" Language="C#" %>
